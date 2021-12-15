789f0965813cca15f997485a6473c6414bf8d75d
Douglas Ferreira <dfaferreira46@gmail.com>
2021-12-15 08:45:23 -0300

    implemented and fixed date and currency formatting

--- script.js
+++ script.js
@@ -1,36 +1,44 @@
 
-// TOGGLE THE TRANSACTIONS MODAL
-const transactionsModal = document.querySelector('.modal-transactions__overlay');
+// TOGGLE THE TRANSACTIONS MODAL VISIBILITY
+const transactionsModal = document.querySelector('.modal-transactions__content');
+
+const transactionsModalOverlay = document.querySelector('.modal-transactions__overlay');
 
 const closeModalButton = document.querySelector('.modal-transactions__close-button');
 
 newTransactionButton.onclick = () => {
-    transactionsModal.toggleAttribute('data-hidden');
+    transactionsModalOverlay.toggleAttribute('data-hidden');
 };
 
 closeModalButton.onclick = () => {
-    transactionsModal.toggleAttribute('data-hidden');
+    transactionsModalOverlay.toggleAttribute('data-hidden');
 };
 
 window.onclick = (event) => {
-    if(event.target == transactionsModal) {
-        transactionsModal.toggleAttribute('data-hidden');
+    if(event.target === transactionsModalOverlay) {
+        transactionsModalOverlay.toggleAttribute('data-hidden');
     }
-  }
+}
+
+window.addEventListener('keydown', (event) => {
+    if(event.key === "Escape") {
+        transactionsModalOverlay.toggleAttribute('data-hidden');
+    }
+})
 
 // TOGGLE CURRENCY PREFIX IN THE TRANSACTION MODAL
 const modalTypeField = document.querySelector('.modal-transactions__type-field');
 
-const modalCurrencyPrefix = document.querySelector('.modal-transactions__currency-prefix');
+const modalCurrencyPrefix = document.querySelector('#amountFieldLabel');
 
 modalTypeField.onchange = () => {
     if(modalTypeField.value == "Expense") {
-        if(modalCurrencyPrefix.classList.contains('currency-prefix')) {
-            modalCurrencyPrefix.classList.replace('currency-prefix', 'currency-prefix-negative');
+        if(modalCurrencyPrefix.classList.contains('modal-transactions__currency-prefix')) {
+            modalCurrencyPrefix.classList.replace('modal-transactions__currency-prefix', 'modal-transactions__currency-prefix-negative');
         }
     } else {
-        if(modalCurrencyPrefix.classList.contains('currency-prefix-negative')) {
-            modalCurrencyPrefix.classList.replace('currency-prefix-negative', 'currency-prefix');
+        if(modalCurrencyPrefix.classList.contains('modal-transactions__currency-prefix-negative')) {
+            modalCurrencyPrefix.classList.replace('modal-transactions__currency-prefix-negative', 'modal-transactions__currency-prefix');
         }
     }
 };
@@ -41,20 +49,20 @@ const transactions = [
         id: 1,
         title: 'Site development',
         date: '04/13/2021',
-        amount: 1200000
+        amount: 1200000,
     },
     {
         id: 2,
         title: 'Rent',
         date: '04/13/2021',
-        amount: 150000
+        amount: -150000,
     },
     {
         id: 3,
         title: 'Netflix subscription',
-        date: '04/13/2021',
-        amount: 5590
-    }
+        date: '06/01/2021',
+        amount: -5590,
+    },
 ];
 
 const stats = {
@@ -70,21 +78,67 @@ const stats = {
 };
 
 // ADD TRANSACTIONS
-addTransactions();
+const transactionsRowsWrapper = document.querySelector('.transactions__table-rows-wrapper');
 
-function addTransactions() {
-    const transactionsTable = document.querySelector('#transactionsTable');
-    transactionsTable.append(transactionInnerHTML("text", "today", 50));
+transactions.forEach(transaction => addTransaction(transaction));
+
+function addTransaction(transaction, index) {
+    const transactionsRow = document.createElement('div');
+    transactionsRow.classList.add('transactions__table-row');
+    transactionsRow.innerHTML = transactionInnerHTML(transaction);
+    transactionsRowsWrapper.appendChild(transactionsRow);
 }
 
-function transactionInnerHTML(title, date, amount) {
+function transactionInnerHTML(transaction) {
+
+    const signStyle = transaction.amount < 0 ? 'minus-prefix' : 'plus-prefix';
+
     const transactionRowTemplate = `
-        <div class="transactions__table-row">
-            <p class="f-body-M f-one-liner transactions__title">${title}</p>
-            <p class="f-body-M f-medium f-one-liner transactions__date">${date}</p>
-            <p class="f-body-M f-medium f-one-liner transactions__amount currency-prefix-positive">${amount}</p>
-        </div>
+        <p class="f-body-M f-one-liner transactions__title">${transaction.title}</p>
+        <p class="f-body-M f-medium f-one-liner transactions__date">${formatDate(transaction.date)}</p>
+        <p class="f-body-M f-medium f-one-liner transactions__amount ${signStyle}">${formatCurrency(transaction.amount)}</p>
     `;
 
     return transactionRowTemplate;
+}
+
+// FORMAT CURRENCY
+function formatCurrency(amount) {
+    const sign = Number(amount) < 0 ? '-' : '';
+
+    amount = String(amount).replace(/\D/g, '');
+    amount = Number(amount) / 100;
+    amount = amount.toLocaleString("en-US", {
+        style: "currency",
+        currency: "BRL",
+    });
+
+    return `${sign} ${amount}`;
+}
+
+// FORMAT DATE
+function formatDate(date) {
+    const dateArray = date.split('/');
+    const formattedDate = `${numberToMonth(dateArray[0])} ${dateArray[1]}, ${dateArray[2]}`;
+
+    return formattedDate;
+}
+
+function numberToMonth(monthNum) {
+    const monthsMMM = {
+        '01': "Jan",
+        '02': "Feb",
+        '03': "Mar",
+        '04': "Apr",
+        '05': "May",
+        '06': "Jun",
+        '07': "Jul",
+        '08': "Aug",
+        '09': "Sep",
+        '10': "Oct",
+        '11': "Nov",
+        '12': "Dec",
+    }
+
+    return monthsMMM[monthNum];
 }
