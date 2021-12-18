bd7c09314438d4f3097612083683d2e3b7f34379
Douglas Ferreira <dfaferreira46@gmail.com>
2021-12-18 11:07:34 -0300

    extracted functions to other files

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
+import { addTransaction, listAllTransactions, clearAllTransactions } from "./transactions.js";
+import { updateStats } from "./stats.js";
 
 // TOGGLE THE TRANSACTIONS MODAL VISIBILITY
-const transactionsModal = document.querySelector('.modal-transactions__content');
-
 const transactionsModalOverlay = document.querySelector('.modal-transactions__overlay');
 
 const closeModalButton = document.querySelector('.modal-transactions__close-button');
@@ -44,7 +44,7 @@ modalTypeField.onchange = () => {
 };
 
 // SET UP TRANSACTIONS
-const transactions = [
+const allTransactions = [
     {
         id: 1,
         title: 'Site development',
@@ -107,115 +107,26 @@ const transactions = [
     },
 ];
 
-// ADD TRANSACTIONS
-const transactionsRowsWrapper = document.querySelector('.transactions__table-rows-wrapper');
+listAllTransactions(allTransactions);
+updateStats(allTransactions);
 
-transactions.forEach(transaction => addTransaction(transaction));
-
-function addTransaction(transaction, index) {
-    const transactionsRow = document.createElement('div');
-    transactionsRow.classList.add('transactions__table-row');
-    transactionsRow.innerHTML = transactionInnerHTML(transaction);
-    transactionsRowsWrapper.appendChild(transactionsRow);
+function createTransaction(transactionData) {
+    addTransaction(transactionData, allTransactions);
+    clearAllTransactions();
+    listAllTransactions(allTransactions);
+    updateStats(allTransactions);
 }
 
-function transactionInnerHTML(transaction) {
+createTransaction({
+    id: 6,
+    title: 'Test',
+    date: '12/18/2021',
+    amount: 15080,
+});
 
-    const signStyle = transaction.amount < 0 ? 'minus-prefix' : 'plus-prefix';
-
-    const transactionRowTemplate = `
-        <p class="f-body-M f-one-liner transactions__title">${transaction.title}</p>
-        <p class="f-body-M f-medium f-one-liner transactions__date">${formatDate(transaction.date)}</p>
-        <p class="f-body-M f-medium f-one-liner transactions__amount ${signStyle}">${formatCurrency(transaction.amount)}</p>
-    `;
-
-    return transactionRowTemplate;
-}
-
-// FORMAT CURRENCY
-function formatCurrency(amount) {
-    const sign = Number(amount) < 0 ? '-' : '';
-
-    amount = String(amount).replace(/\D/g, '');
-    amount = Number(amount) / 100;
-    amount = amount.toLocaleString("en-US", {
-        style: "currency",
-        currency: "BRL",
-    });
-
-    return `${sign} ${amount.replace('R$', 'R$ ')}`;
-}
-
-// FORMAT DATE
-function formatDate(date) {
-    const dateArray = date.split('/');
-    const formattedDate = `${numberToMonth(dateArray[0])} ${dateArray[1]}, ${dateArray[2]}`;
-
-    return formattedDate;
-}
-
-function numberToMonth(monthNum) {
-    const monthsMMM = {
-        '01': "Jan",
-        '02': "Feb",
-        '03': "Mar",
-        '04': "Apr",
-        '05': "May",
-        '06': "Jun",
-        '07': "Jul",
-        '08': "Aug",
-        '09': "Sep",
-        '10': "Oct",
-        '11': "Nov",
-        '12': "Dec",
-    }
-
-    return monthsMMM[monthNum];
-}
-
-// CALCULATE STATS
-let stats = {
-    balance: 0,
-    transactions: 0,
-    income: 0,
-    expenses: 0,
-};
-
-stats.balance = transactions.reduce((acc, cur) => {
-    return acc + cur.amount;
-}, 0);
-
-stats.transactions = transactions.length;
-
-stats.income = transactions.reduce((acc, cur) => {
-    if(cur.amount > 0) {
-        return acc + cur.amount;
-    } else {
-        return acc + 0;
-    }
-}, 0);
-
-stats.expenses = transactions.reduce((acc, cur) => {
-    if(cur.amount < 0) {
-        return acc + cur.amount;
-    } else {
-        return acc + 0;
-    }
-}, 0);
-
-// UPDATE STATS ON UI
-const balance = document.querySelector('.primary-stats__amount');
-balance.innerText = formatCurrency(stats.balance);
-
-const transactionsCount = document.querySelector('#transactionsCount');
-if(stats.transactions > 0) {
-    transactionsCount.innerText =  stats.transactions < 9 ? '0' + stats.transactions + ' transactions' : stats.transactions + ' transactions';
-} else {
-    transactionsCount.innerText = 'No transactions';
-}
-
-const income = document.querySelector('.secondary-stats__income-amount');
-income.innerText = formatCurrency(stats.income);
-
-const expenses = document.querySelector('.secondary-stats__expenses-amount');
-expenses.innerText = formatCurrency(stats.expenses);
+createTransaction({
+    id: 7,
+    title: 'Test 2',
+    date: '12/20/2021',
+    amount: 550,
+});
