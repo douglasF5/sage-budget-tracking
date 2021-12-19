d15ec92669af2394ad753de5b7a6ecd831d84ee1
Douglas Ferreira <dfaferreira46@gmail.com>
2021-12-19 13:57:48 -0300

    updated the HTML template for each table row

--- transactions.js
+++ transactions.js
@@ -13,7 +13,10 @@ function listTransaction(transaction, index) {
 function transactionInnerHTML(transaction) {
     const signStyle = transaction.amount < 0 ? 'minus-prefix' : 'plus-prefix';
     const transactionRowTemplate = `
-        <p class="f-body-M f-one-liner transactions__title">${transaction.title}</p>
+        <div class="transactions__cell-wrapper">
+            <button class="icon-button transactions__delete-button" data-hidden><svg class="transactions__delete-button-icon"><use xlink:href="assets/icons.svg#x-icon" /></svg></button>
+            <p class="f-body-M f-one-liner transactions__title">${transaction.title}</p>
+        </div>
         <p class="f-body-M f-medium f-one-liner transactions__date">${formatDate(transaction.date)}</p>
         <p class="f-body-M f-medium f-one-liner transactions__amount ${signStyle}">${formatCurrency(transaction.amount)}</p>
     `;
