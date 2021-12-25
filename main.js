f31477318d290dad377e775042682adeaadc3666
Douglas Ferreira <dfaferreira46@gmail.com>
2021-12-25 11:17:32 -0300

    refactored a couple of functions

--- main.js
+++ main.js
@@ -1,63 +1,19 @@
-import { addTransaction, listAllTransactions, clearAllTransactions } from "./transactions.js";
-import { updateStats } from "./stats.js";
+import { transactionElements, createTransaction, clearTransaction, renderAllTransactions, clearAllTransactions, toggleDeleteMode } from "./transactions.js";
+import { updateStatsOnUI } from "./stats.js";
+import { modal, showModal, hideModal, changeAmountFieldColor, changeAmountFieldPrefix } from "./modal.js";
+import { showClickedElement } from "./utils.js";
 
 // TOGGLE THE TRANSACTIONS MODAL VISIBILITY
-const transactionsModalOverlay = document.querySelector('.modal-transactions__overlay');
+transactionElements.newTransactionButtonWrapper.onclick = showModal;
+modal.closeButton.onclick = hideModal;
+modal.overlay.addEventListener('click', event => hideModal(event, true));
+window.addEventListener('keydown', event => hideModal(event, false));
 
-const closeModalButton = document.querySelector('.modal-transactions__close-button');
+// SET UP AMOUNT FIELD
+modal.amountField.oninput = changeAmountFieldColor;
+modal.typeField.onchange = changeAmountFieldPrefix;
 
-const newTransactionButton = document.querySelector('#newTransactionButton');
-
-newTransactionButton.onclick = () => {
-    transactionsModalOverlay.toggleAttribute('data-hidden');
-};
-
-closeModalButton.onclick = () => {
-    transactionsModalOverlay.toggleAttribute('data-hidden');
-};
-
-window.onclick = (event) => {
-    if(event.target === transactionsModalOverlay) {
-        transactionsModalOverlay.toggleAttribute('data-hidden');
-    }
-}
-
-window.addEventListener('keydown', (event) => {
-    if(event.key === "Escape") {
-        transactionsModalOverlay.toggleAttribute('data-hidden');
-    }
-})
-
-// TOGGLE CURRENCY PREFIX IN THE TRANSACTION MODAL
-const modalTypeField = document.querySelector('.modal-transactions__type-field');
-
-const modalCurrencyPrefix = document.querySelector('#amountFieldLabel');
-
-const amountField = document.getElementById('amount-field');
-
-amountField.oninput = () => {
-    if(amountField.value !== '') {
-        modalCurrencyPrefix.classList.add('lit');
-    } else {
-        modalCurrencyPrefix.classList.remove('lit');
-    }
-}
-
-
-modalTypeField.onchange = () => {
-
-    if(modalTypeField.value == "Expense") {
-        if(modalCurrencyPrefix.classList.contains('modal-transactions__currency-prefix')) {
-            modalCurrencyPrefix.classList.replace('modal-transactions__currency-prefix', 'modal-transactions__currency-prefix-negative');
-        }
-    } else {
-        if(modalCurrencyPrefix.classList.contains('modal-transactions__currency-prefix-negative')) {
-            modalCurrencyPrefix.classList.replace('modal-transactions__currency-prefix-negative', 'modal-transactions__currency-prefix');
-        }
-    }
-};
-
-// SET UP TRANSACTIONS
+// TRANSACTIONS DATA
 const allTransactions = [
     {
         id: 1,
@@ -121,71 +77,25 @@ const allTransactions = [
     },
 ];
 
-listAllTransactions(allTransactions);
-updateStats(allTransactions);
+// SET UP THE DATA ON THE FIRST PAGE LOAD
+renderAllTransactions(allTransactions);
+updateStatsOnUI(allTransactions);
 
-function createTransaction(transactionData) {
-    addTransaction(transactionData, allTransactions);
-    clearAllTransactions();
-    listAllTransactions(allTransactions);
-    updateStats(allTransactions);
-}
-
-function deleteTransaction(index, element) {
-    allTransactions.splice(index, 1);
-    element.remove();
-    updateStats(allTransactions);
-}
+// DELETE TRANSACTION
+transactionElements.editButtonWrapper.onclick = toggleDeleteMode;
 
 createTransaction({
     id: 6,
     title: 'Test',
     date: '12/18/2021',
     amount: 15080,
-});
+}, allTransactions);
 
 createTransaction({
     id: 7,
     title: 'Test 2',
     date: '12/20/2021',
     amount: 550,
-});
+}, allTransactions);
 
-// DELETE TRANSACTIONS
-const editButton = document.getElementById('editTransactionButton');
-const editButtonWrapper = editButton.closest('li');
-const cellWrappers = document.querySelectorAll('.transactions__cell-wrapper');
-
-console.log(editButtonWrapper);
-
-editButtonWrapper.onclick = () => {
-
-    if(editButton.innerText === 'Edit') {
-        editButton.innerText = 'Done';
-        newTransactionButton.disabled = true;
-    } else {
-        editButton.innerText = 'Edit';
-        newTransactionButton.disabled = false;
-    }
-
-    for(let element of cellWrappers) {
-        element.toggleAttribute('data-visible');
-    }
-}
-
-
-const tableRowsWrapper = document.querySelector('.transactions__table-rows-wrapper').children;
-
-const deleteButtons = document.getElementsByClassName('transactions__delete-button');
-
-Object.values(deleteButtons).forEach(button => {
-    button.addEventListener('click', () => {
-        const row = button.closest('div.transactions__table-row');
-        deleteTransaction(getElementKey(tableRowsWrapper, row), row);
-    });
-});
-
-function getElementKey(object, value) {
-    const key = Object.keys(object).find(key => object[key] === value);
-    return key;
-}
+export { allTransactions };
